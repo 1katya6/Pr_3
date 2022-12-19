1c0b632debb28159f21ecd4662513ee735986c4a
1katya6 <98809518+1katya6@users.noreply.github.com>
2022-12-19 21:38:15 +0500

    Add files via upload

---
+++ Pr_3/Pr_3/SignCount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr_3 
+{
+    public class SignCount
+    {
+        public int plusCount = 0;
+        public int minusCount = 0;
+        public void Counting(TreeNode root)
+        {
+            if (root == null)
+                return;
+
+            if (root.Left != null)
+                Counting(root.Left);
+
+            if (root.Info >= 0)
+                plusCount++;
+            else
+                minusCount++;
+
+            if (root.Right != null)
+                Counting(root.Right);
+        }
+    }
+}
